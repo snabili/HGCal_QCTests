95e602ca5dc54749cf8bb820ebba192998062658
Mekhpar <mekhala.paranjpe@gmail.com>
2023-11-14 13:07:43 -0500

    Update i2c-server.sh

--- i2c-server.sh
+++ i2c-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hostarea="~/sw_0717" #Check this before running
+hostarea="~/sw_0717/" #Check this before running
 cd $hostarea"hexactrl-sw/zmq_i2c/gbt-sca-sw/"
 source ./env.sh
 cd ../
