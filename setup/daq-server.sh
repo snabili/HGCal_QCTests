c218706d49569b964d11efe3d5bce921d838465e
Mekhpar <mekhala.paranjpe@gmail.com>
2023-11-14 13:21:51 -0500

    Update daq-server.sh

--- setup/daq-server.sh
+++ setup/daq-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hostarea="~/sw_0717" #Check this before running
+hostarea="/home/HGCAL_dev/sw_0717/" #Check this before running
 export LD_LIBRARY_PATH=/opt/cactus/lib:$LD_LIBRARY_PATH # You can add this to ~/.bashrc if needed
 cd $hostarea"hexactrl-sw/build"
 ./daq-server -f /opt/cms-hgcal-firmware/hgc-test-systems/active/uHAL_xml/connections.xml
